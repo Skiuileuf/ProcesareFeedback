49472d93d05dd76e7dbbdc67aa2575ae43a06176
Skiuileuf <viziteu.cezar@gmail.com>
2022-01-28 10:07:23 +0200

    Update ReportController.cs

--- ReportController.cs
+++ ReportController.cs
@@ -13,7 +13,11 @@ namespace ProcesareFeedback
         public static void GenerateReport(TeacherReport report)
         {
             var ViewPath = $"D:\\Desktop\\Report.cs.docx"; //template.cs.docx
-            var documentPath = $"D:\\Desktop\\feedback\\{report.NCLASA}{report.LCLASA}-{report.PROFESOR}-{report.DISCIPLINA}.docx";//result.docx
+            var documentFolder = $"D:\\Desktop\\Feedback{report.NCLASA}{report.LCLASA}";
+            if (!Directory.Exists(documentFolder)) Directory.CreateDirectory(documentFolder);
+            var documentName = $"{report.NCLASA}{report.LCLASA}-{report.DISCIPLINA.ToUpperInvariant()}-{report.PROFESOR}.docx";
+            //var documentPath = $"D:\\Desktop\\feedback\\";//result.docx
+            var documentPath = Path.Combine(documentFolder, documentName);
             //var model = CreateViewModel(report);
             var model = report;
 
